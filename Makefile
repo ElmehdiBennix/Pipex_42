35be6603b2aa9cf7a0fa1482f29aa1be75de3e35
ElmehdiBennix <bennixmehdi@gmail.com>
2023-02-01 15:42:48 +0100

    commit

--- Makefile
+++ Makefile
@@ -1,39 +1,34 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    Makefile                                           :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: bennix <bennix@student.42.fr>              +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2023/01/28 19:07:45 by ebennix           #+#    #+#              #
-#    Updated: 2023/02/01 01:28:46 by bennix           ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
+AR_NAME := pipex.a
 NAME := pipex
+
 CC := gcc
 CFLAGS := -g -Wall -Wextra -Werror
 
 HEADER := pipex.h
 FILES := pipex /
 		pipex_utils /
+
+
 SRC := $(FILES:=.c)
 OBJ:= $(SRC:.c=.o)
 
-AR := ar rc
+
+AR := ar -rc
 RM := rm -rf
 
 
 all :$(NAME)
 
 $(NAME):$(OBJ)
-	$(AR) $(NAME) $(OBJ)
+	$(cc) $(OBJ) -o $(NAME)
+#$(cc) $(OBJ) main.c -o $(NAME) 
 
-%.o : %.c $(HEADER)
+
+%.o : $(SRC) $(HEADER)
 	$(CC) $(CFLAGS) -c $< -o $@
 
-exe : $(SRC) $(HEADER)
-	$(CC) $(CFLAGS) $(SRC) -o pipex
+ar : $(OBJ)
+	$(AR) $(AR_NAME) $(OBJ)
 
 clean :
 	$(RM) $(OBJ)
--- pipex.c
+++ pipex.c
@@ -3,18 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ebennix <ebennix@student.42.fr>            +#+  +:+       +#+        */
+/*   By: bennix <bennix@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/28 19:07:50 by ebennix           #+#    #+#             */
-/*   Updated: 2023/01/31 00:57:55 by ebennix          ###   ########.fr       */
+/*   Updated: 2023/02/01 14:59:01 by bennix           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-void    pipex(int fd1, char* cmd1, char* cmd2, int fd2) // for child processes / char *cmd1, char *cmd2, 
+void    pipex(int fd1, char* cmd1, char* cmd2, int fd2) // for child processes / char *cmd1, outs put it to parent as input pipe to char *cmd2, to finish 
 {
-    
+    // for process to child and parrent split the tasks so that the input for the child finishes as input for the parent to finish to simulate a pipe 
+    //note / wait for child to finish the exec
 }
 int main (int ac , char **av) // 0 for a.out next for next in exec
 {
@@ -30,6 +31,7 @@ int main (int ac , char **av) // 0 for a.out next for next in exec
         perror("Error opening W/R files");
         return 1;
     }
+    execve();
     pipex(fd[0],,,fd[1]);
     return 0;
 }
